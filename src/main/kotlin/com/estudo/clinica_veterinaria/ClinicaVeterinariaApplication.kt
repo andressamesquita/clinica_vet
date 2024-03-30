cc7c404b30f729f2f837e3fca6404051f337437d
andressamesquita <rib.mesq.andressa72@gmail.com>
2024-03-30 14:27:23 -0300

    :tada: init: setup inicial

---
+++ src/main/kotlin/com/estudo/clinica_veterinaria/ClinicaVeterinariaApplication.kt
@@ -0,0 +1,11 @@
+package com.estudo.clinica_veterinaria
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClinicaVeterinariaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClinicaVeterinariaApplication>(*args)
+}
---
+++ src/test/kotlin/com/estudo/clinica_veterinaria/ClinicaVeterinariaApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.estudo.clinica_veterinaria
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClinicaVeterinariaApplicationTests {
+
+	@Test
+	fun contextLoads(): String {
+		return "vamo testar"
+	}
+
+}
